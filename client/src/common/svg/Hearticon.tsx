d4ec4fcb5a6b8ea37de6c8e073eae7b5ced1773f
Louis <roccalouis26@gmail.com>
2025-02-27 16:20:29 +0100

    refactor: like and dislike

--- client/src/common/svg/Hearticon.tsx
+++ client/src/common/svg/Hearticon.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function HeartIcon({
   isActive,
   handleIsLiked,
@@ -6,34 +8,20 @@ export default function HeartIcon({
   handleIsLiked: () => void;
 }) {
   return (
-    <>
-      <style>
-        {`
-        .heart-icon {
-          cursor: pointer;
-        }
-        
-        .heart-icon:hover path {
-          stroke: red;
-        }
-      `}
-      </style>
-      <svg
-        viewBox=" 0 0 256 256"
-        height={26}
-        width={26}
-        xmlns="http://www.w3.org/2000/svg"
-        fill={isActive ? "red" : "none"}
-        className="heart-icon"
-        onClick={handleIsLiked}
-      >
-        <rect fill="none" height="26" width="26" />
-        <path
-          strokeWidth={12}
-          stroke={isActive ? "red" : "rgb(90, 90, 90)"}
-          d="M176,32a60,60,0,0,0-48,24A60,60,0,0,0,20,92c0,71.9,99.9,128.6,104.1,131a7.8,7.8,0,0,0,3.9,1,7.6,7.6,0,0,0,3.9-1,314.3,314.3,0,0,0,51.5-37.6C218.3,154,236,122.6,236,92A60,60,0,0,0,176,32Z"
-        />
-      </svg>
-    </>
+    <svg
+      viewBox=" 0 0 256 256"
+      height={26}
+      width={26}
+      xmlns="http://www.w3.org/2000/svg"
+      fill={isActive ? "red" : "none"}
+      onClick={handleIsLiked}
+    >
+      <rect fill="none" height="26" width="26" />
+      <path
+        strokeWidth={12}
+        stroke={isActive ? "none" : "rgb(90, 90, 90)"}
+        d="M176,32a60,60,0,0,0-48,24A60,60,0,0,0,20,92c0,71.9,99.9,128.6,104.1,131a7.8,7.8,0,0,0,3.9,1,7.6,7.6,0,0,0,3.9-1,314.3,314.3,0,0,0,51.5-37.6C218.3,154,236,122.6,236,92A60,60,0,0,0,176,32Z"
+      />
+    </svg>
   );
 }
