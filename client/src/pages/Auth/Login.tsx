8e5941235396f793780907fc6e2ec24162db7188
Louis <roccalouis26@gmail.com>
2025-02-27 16:21:04 +0100

    refactor: like and dislike

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from "./Auth.module.css";
 import LoginForm from "../../components/AuthForm/LoginForm";
 
@@ -8,23 +7,6 @@ export default function Login() {
       <section>
         <LoginForm />
       </section>
-=======
-import {useState} from "react";
-import styles from "./Auth.module.css";
-import HeartIcon from "../../common/svg/HeartIcon";
-
-import AuthForm from "../../components/AuthForm/AuthForm";
-export default function Login() {
-  const [isLiked, setIsLiked] = useState(false);
-
-  return (
-    <div className={styles.authBody}>
-      <section>
-        <AuthForm type="login" />
-      </section>
-
-      <HeartIcon handleIsLiked={() => setIsLiked(!isLiked)} isActive={isLiked} />
->>>>>>> c65cf40 (add: auth page)
     </div>
   );
 }
